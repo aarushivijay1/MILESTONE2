62dfed1510f168f54a18ca6bdd81392ff0ebd326
nickrich1 <50879540+nickrich1@users.noreply.github.com>
2019-10-23 17:04:58 -0400

    Update Milestone2Main.java

--- Milestone2Main.java
+++ Milestone2Main.java
@@ -2,18 +2,19 @@ public class Milestone2Main {
 
 	public static void main (String []args) throws Exception{ 
 		
-		System.out.println("Hi");
-		Milestone2Article a = new Milestone2Article();
-		String nytimes = "/Users/nicholasrichardson/Desktop/MILESTONE2-master/guardian.txt";
-		a.runArticles(nytimes);
-        System.out.println(a.runArticles(nytimes));
+	Milestone2Article a = new Milestone2Article();
+	String nytimes = "/Users/nicholasrichardson/Desktop/MILESTONE2-master/guardian.txt";
         Milestone2Counter c = new Milestone2Counter();
-        System.out.println("statistics of NY Times Article:");
-        c.Counter(nytimes);
-        c.removeStopWords(nytimes);
-        Milestone2freqtest f = new Milestone2freqtest();
-        f.Frequency();
-        System.out.println("--------------------");
+        String text = a.runArticles(nytimes);
+        c.Counter(text);
+        Milestone2StopWordEliminator nostopwords = new Milestone2StopWordEliminator();
+        String nostopwordsstring = nostopwords.RemoveStopWords(text);
+	//Pull from this string for the sentiment analysis	
+		
+		
+		
+        System.out.println("The following are the statistics without stop words");
+        c.Counter(nostopwordsstring);
         
 
 
